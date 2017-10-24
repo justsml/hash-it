c4e16407a3fe048ee1823d54133e7b256fde106d
Kyle Reubendale <kylereubendale@Kyles-MacBook-Air.local>
2017-10-24 10:12:35 -0600

    bcrypt

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ module.exports = function hashString(str, callback) {
   callback(null, result)
   console.log(result)
 }
+
+
+module.exports = function hashString(str, callback) {
+var bcrypt = require('bcrypt')
+const saltRounds = 10;
+const password = str;
+var salt = bcrypt.genSaltSync(saltRounds);
+var hash = bcrypt.hashSync(password, salt)
+console.log(hash)
+  callback(null, hash)
+
+}
