3f29d787d5d36648f49e9c061eb4a9105f653bb3
Chelcie Britt <chelciebritt@Chelcies-MacBook-Pro.local>
2017-10-24 10:03:55 -0600

    hashish

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
+var passwordHash = require('password-hash');
 module.exports = function hashString(str, callback) {
-  // Add hashing function here
-  // pass result using node standard pattern:
-  // callback(error, result) // where result is the hash
-  callback(null, str)
-}
+  var hashedPassword = passwordHash.generate(str);
+  callback(null, hashedPassword);
+};
