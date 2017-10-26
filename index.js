308216be19c4649b031982494cabb20ddfc45f27
JOSHUA SAUNDERS <joshnsaunders@gmail.com>
2017-10-26 13:47:20 -0600

    cracked the hash

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+var md5 = require('md5');
+
+
 module.exports = function hashString(str, callback) {
+
+
+let result = (md5(str));
+console.log(result);
+// callback(error, result)
+
+
   // Add hashing function here
   // pass result using node standard pattern:
   // callback(error, result) // where result is the hash
-  callback(null, str)
+callback(null, result)
 }
